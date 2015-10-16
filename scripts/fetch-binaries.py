e4bcabe3f4de902bc87f12503c9ee2c6b898958f
Vincent Lang <vincent@plexapp.com>
2015-10-16 17:37:37 +0200

    Bump deps
    ('Im not even sure if I still need to do this.)

--- scripts/fetch-binaries.py
+++ scripts/fetch-binaries.py
@@ -16,7 +16,7 @@ import glob
 
 # Edit these to set a new default dependencies build
 default_tag = "auto"
-default_release_build_number = "96"
+default_release_build_number = "97"
 default_release_dir = "plexmediaplayer-dependencies"
 default_branch = "master"
 

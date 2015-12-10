01b58f8f8a61ce9f9d0e4e5acc49a8a69564c523
Tobias Hieta <tobias@plexapp.com>
2015-12-10 15:43:47 +0100

    Don't build windows version with QTROOT

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -1,6 +1,6 @@
 cd %BUILD_DIR%  || exit /b
 
-%CMAKE_DIR%\cmake -DCRASHDUMP_SECRET=%CD_SECRET% -DQTROOT=%QTROOT% -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=output .. -G Ninja -DCODE_SIGN=ON || exit /b
+%CMAKE_DIR%\cmake -DCRASHDUMP_SECRET=%CD_SECRET% -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=output .. -G Ninja -DCODE_SIGN=ON || exit /b
 
 ninja || exit /b
 ninja package || exit /b

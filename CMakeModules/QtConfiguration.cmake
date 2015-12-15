ce29d39be707df95673a62ce95e7ab4d8928be2e
Tobias Hieta <tobias@plex.tv>
2015-12-15 09:55:16 +0100

    Remove Qt background property configure check since we now require Qt 5.6

--- CMakeModules/QtConfiguration.cmake
+++ CMakeModules/QtConfiguration.cmake
@@ -54,25 +54,3 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${QT5_CFLAGS}")
 
 set(CMAKE_REQUIRED_INCLUDES ${Qt5WebEngine_INCLUDE_DIRS};${Qt5WebEngine_PRIVATE_INCLUDE_DIRS})
 set(CMAKE_REQUIRED_LIBRARIES ${QT5_LIBRARIES})
-
-OPTION(SKIP_QT_TEST "Skip tests for required Qt features" OFF)
-
-if(NOT SKIP_QT_TEST)
-	include(CheckCXXSourceCompiles)
-        check_cxx_source_compiles("
-                #include <private/qquickwebengineview_p.h>
-                #include <QColor>
-                int main()
-                {
-                QQuickWebEngineView* view = new QQuickWebEngineView(NULL);
-                view->setBackgroundColor(QColor(\"transparent\"));
-                }
-	" WebEngineBackgroundProperty)
-
-	if(NOT WebEngineBackgroundProperty)
-		message(FATAL_ERROR "QQuickWebEngineView doesn't have the background property."
-			"This will break video playback. As of Qt 5.5 you need to manually patch and build Qt to get this property."
-			"With the release of Qt5.6 it will no longer be required. See qt-patches/README for more details.")
-	endif(NOT WebEngineBackgroundProperty)
-
-endif(NOT SKIP_QT_TEST)

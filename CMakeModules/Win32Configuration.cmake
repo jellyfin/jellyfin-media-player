6ad13e0b111d695ea4b04010e61688e5916be548
Tobias Hieta <tobias@plex.tv>
2016-02-17 09:30:15 +0100

    Set compiler flags based on compiler_id not IDE used

--- CMakeModules/Win32Configuration.cmake
+++ CMakeModules/Win32Configuration.cmake
@@ -2,8 +2,13 @@ set(INSTALL_BIN_DIR .)
 set(INSTALL_RESOURCE_DIR resources)
 set(HAVE_UPDATER 1)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /Oy-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Oy-")
+if(${CMAKE_C_COMPILER_ID} STREQUAL MSVC)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /Oy-")
+endif()
+
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Oy-")
+endif()
 
 find_library(WINMM winmm)
 find_library(IMMLIB imm32)

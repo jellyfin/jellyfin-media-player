9364c9083b3a1fb474ac9981320d3998274aea6f
Tobias Hieta <tobias@plexapp.com>
2016-04-06 10:33:22 +0200

    Don't crash if g_qmlEngine is not available on destruction

--- src/ui/Globals.cpp
+++ src/ui/Globals.cpp
@@ -44,7 +44,8 @@ void Globals::SetContextProperty(const QString& property, const QVariant& value)
 /////////////////////////////////////////////////////////////////////////////////////////
 void Globals::EngineDestroy()
 {
-  delete g_qmlEngine;
+  if (g_qmlEngine)
+    delete g_qmlEngine;
   g_qmlEngine = nullptr;
 }
 

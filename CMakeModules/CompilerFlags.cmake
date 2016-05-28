4bc5a76b9471019c692151e3c589da9c6f2328be
Tobias Hieta <tobias@hieta.se>
2016-05-28 13:31:20 +0200

    Better fix for 9e4f99e1b4135e02dbb7e5779bb165efbd6db574
    This sets the CMAKE_EXE_LINKER_FLAGS_<BUILD_TYPE> instead of setting
    CMAKE_EXE_LINKER_FLAGS based on what the current BUILD_TYPE is. With
    multi-configuration IDEs the former breaks.

--- CMakeModules/CompilerFlags.cmake
+++ CMakeModules/CompilerFlags.cmake
@@ -13,13 +13,13 @@ enable_if_supported(COMPILER_FLAGS_THIRD_PARTY "-Wno-shorten-64-to-32")
 enable_if_supported(COMPILER_FLAGS_THIRD_PARTY "/wd4244")
 enable_if_supported(COMPILER_FLAGS_THIRD_PARTY "/wd4267")
 
-string(TOLOWER "${CMAKE_BUILD_TYPE}" build_type_lower)
-if (NOT (build_type_lower MATCHES "debug"))
-  enable_if_links(LINK_FLAGS "-flto")
-endif()
-
+enable_if_links(LINK_FLAGS_RELEASE "-flto")
 enable_if_links(LINK_FLAGS "-fuse-ld=gold")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILER_FLAGS}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILER_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LINK_FLAGS}")
+
+# release link flags
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} ${LINK_FLAGS_RELEASE}")
+set(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO} ${LINK_FLAGS_RELEASE}")

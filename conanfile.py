9d5414554ec81c604b72652f20182a1fdd2284ca
Tobias Hieta <tobias@plexapp.com>
2016-11-18 09:21:21 +0100

    Bump desktop version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 class PlexMediaPlayer(ConanFile):
   settings = "os", "compiler", "build_type", "arch"
   requires = (
-    "web-client-desktop/2.10.10-a249d92@plex/stable",
+    "web-client-desktop/2.10.10-1053583@plex/stable",
     "web-client-tv/2.10.8-719c7dd@plex/stable"
   )
   

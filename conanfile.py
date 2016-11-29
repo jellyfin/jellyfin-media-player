dc029f5506ae4a3d5459756826e54ea413203dc1
Tobias Hieta <tobias@plexapp.com>
2016-11-29 12:56:50 +0100

    Tweak how we include the web-client from conan

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,12 @@ import os
 
 class PlexMediaPlayer(ConanFile):
   settings = "os", "compiler", "build_type", "arch"
-  requires = (
-    "web-client-desktop/2.11.1-db2136e@plex/stable",
-    "web-client-tv/2.10.8-7d6b907@plex/stable"
-  )
+  options = {"include_desktop": [True, False]}
+  default_options = "include_desktop=True"
+  generators = "cmake"
 
-  def imports(self):
-    self.copy("*", dst="web-client/tv", src="tv")
-    self.copy("*", dst="web-client/desktop", src="desktop")
+  def requirements(self):
+    self.requires("web-client-tv/2.10.8-7d6b907@plex/stable")
+
+    if self.options.include_desktop:
+      self.requires("web-client-desktop/2.11.1-db2136e@plex/stable")

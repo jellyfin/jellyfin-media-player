493c25da85e0cfeb4f0522d87284877c5fc1ff22
Eric Matthys <ericmatthys@gmail.com>
2016-12-14 08:42:40 -0700

    Updated web clients

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class PlexMediaPlayer(ConanFile):
   generators = "cmake"
 
   def requirements(self):
-    self.requires("web-client-tv/2.10.8-fa2852f@plex/public")
+    self.requires("web-client-tv/2.10.8-3a42321@plex/public")
 
     if self.options.include_desktop:
-      self.requires("web-client-desktop/2.12.4-2c8649e@plex/public")
+      self.requires("web-client-desktop/2.12.4-b545160@plex/public")

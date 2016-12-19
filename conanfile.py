fc594ee1b476194d844ae876a68cf5c08bb421d4
Eric Matthys <ericmatthys@gmail.com>
2016-12-19 07:27:48 -0700

    Update TV app to ade70eb Fixes #481

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class PlexMediaPlayer(ConanFile):
   generators = "cmake"
 
   def requirements(self):
-    self.requires("web-client-tv/2.10.8-3c44295@plex/public")
+    self.requires("web-client-tv/2.10.8-ade70eb@plex/public")
 
     if self.options.include_desktop:
       self.requires("web-client-desktop/2.12.5-9b613e8@plex/public")

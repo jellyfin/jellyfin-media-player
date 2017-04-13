8cf398a0ba7b0be35dee2316bf3bc23fa81caeed
Mark McDowall <markus.mcd5@gmail.com>
2017-04-13 09:09:03 -0700

    Bumped desktop to 3.4.0-dcef613 and tv to 3.0.1-58dd77c

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class PlexMediaPlayer(ConanFile):
   generators = "cmake"
 
   def requirements(self):
-    self.requires("web-client-tv/2.13.1-be42e24@plex/public")
+    self.requires("web-client-tv/3.0.1-58dd77c@plex/public")
 
     if self.options.include_desktop:
-      self.requires("web-client-desktop/3.0.1-dd2abb1@plex/public")
+      self.requires("web-client-desktop/3.4.0-dcef613@plex/public")

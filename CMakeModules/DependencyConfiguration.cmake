67ea1ddd55f27af59d1d4b0c2e3714e68bce0aa7
Vincent Lang <vincent@plexapp.com>
2017-06-01 15:20:36 +0200

    Bump deps
    Just for something minor.

--- CMakeModules/DependencyConfiguration.cmake
+++ CMakeModules/DependencyConfiguration.cmake
@@ -9,7 +9,7 @@ if(DEPENDENCY_TOKEN)
     set(DEPS_BUILD_NUMBER 86)
   elseif(APPLE OR WIN32)
     set(DEPENDCY_FOLDER plexmediaplayer-dependencies-codecs)
-    set(DEPS_BUILD_NUMBER 241)
+    set(DEPS_BUILD_NUMBER 243)
   endif()
   if(NOT (DEPENDCY_FOLDER STREQUAL ""))
     download_deps(

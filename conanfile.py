a91a2cb1834ed7e110a4e1f006f6af543e594a2b
Matt Seeley <mattseeley@hotmail.com>
2017-07-13 18:38:09 -1000

    Update TV2 and Desktop build
    Desktop build points to v3 player.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class PlexMediaPlayer(ConanFile):
   generators = "cmake"
 
   def requirements(self):
-    self.requires("web-client-tv2/3.12.0-a094a251@plex/stable")
+    self.requires("web-client-tv2/3.13.0-8d434009@plex/stable")
 
     if self.options.include_desktop:
-      self.requires("web-client-desktop/3.10.0-efd1768@plex/public")
+      self.requires("web-client-desktop/3.12.0-b3abe11@plex/public")

3936f63289f751959926d87f09c53fca90201daf
Matt Seeley <mattseeley@hotmail.com>
2017-07-25 16:07:10 -1000

    Set version to 2.0.6 and update TV 2 build

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class PlexMediaPlayer(ConanFile):
   generators = "cmake"
 
   def requirements(self):
-    self.requires("web-client-tv2/3.16.0-87b18d1a@plex/stable")
+    self.requires("web-client-tv2/3.16.1-9e71769d@plex/stable")
 
     if self.options.include_desktop:
       self.requires("web-client-desktop/3.9.1-85417e0@plex/public")

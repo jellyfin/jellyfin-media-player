eee80077a88c143389b8c12cb31b72f2813ca47a
Mark McDowall <mark@mcdowall.ca>
2017-10-26 08:52:02 -0700

    Bumped desktop to 3.26.2-3d9c616

--- conanfile.py
+++ conanfile.py
@@ -11,4 +11,4 @@ class PlexMediaPlayer(ConanFile):
     self.requires("web-client-tv/3.13.3-5401c32@plex/public")
 
     if self.options.include_desktop:
-      self.requires("web-client-desktop/3.21.2-8b92636@plex/public")
+      self.requires("web-client-desktop/3.26.2-3d9c616@plex/public")

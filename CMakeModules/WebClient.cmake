1ef7aca668b1b6f4bd2d30aa629ebe1773e0d2eb
Paulo Coelho Alves <paulo@pcalv.es>
2018-07-24 21:01:58 +0100

    Bumped web-client to 58-fbf73e96584c03

--- CMakeModules/WebClient.cmake
+++ CMakeModules/WebClient.cmake
@@ -4,7 +4,7 @@ option(WEB_CLIENT_TV_OLD "" OFF)
 option(WEB_CLIENT_DISABLE_DESKTOP "" OFF)
 
 # This is the line to edit when you bump the web-client.
-set(WEB_CLIENT_BUILD_ID 57-fc9331fff0ca26)
+set(WEB_CLIENT_BUILD_ID 58-fbf73e96584c03)
 
 get_content_of_url(
   URL "https://artifacts.plex.tv/web-client-pmp/${WEB_CLIENT_BUILD_ID}/buildid.cmake"

7f19c1d6c3f529164c3266c564ddea0e3dce7519
Matt Seeley <mattseeley@hotmail.com>
2020-03-04 10:45:01 -1000

    Bumped web-client to 172-17d1db2564f6ac (attempt 2)
    Eliminate some tricksy whitespace in the build id.

--- CMakeModules/WebClient.cmake
+++ CMakeModules/WebClient.cmake
@@ -4,7 +4,7 @@ option(WEB_CLIENT_TV_OLD "" OFF)
 option(WEB_CLIENT_DISABLE_DESKTOP "" OFF)
 
 # This is the line to edit when you bump the web-client.
-set(WEB_CLIENT_BUILD_ID ​172-17d1db2564f6ac)
+set(WEB_CLIENT_BUILD_ID 172-17d1db2564f6ac)
 
 get_content_of_url(
   URL "https://artifacts.plex.tv/web-client-pmp/${WEB_CLIENT_BUILD_ID}/buildid.cmake"

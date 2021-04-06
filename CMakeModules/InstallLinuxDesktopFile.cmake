8cf45895cf8043a5dd58c6eaf2d66c940c85b614
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-04-06 18:04:50 +0300

    Update icon name

--- CMakeModules/InstallLinuxDesktopFile.cmake
+++ CMakeModules/InstallLinuxDesktopFile.cmake
@@ -1,3 +1,3 @@
 install (FILES ${CMAKE_SOURCE_DIR}/resources/meta/com.github.iwalton3.jellyfin-media-player.desktop DESTINATION ${CMAKE_INSTALL_FULL_DATAROOTDIR}/applications)
 install (FILES ${CMAKE_SOURCE_DIR}/resources/meta/com.github.iwalton3.jellyfin-media-player.appdata.xml DESTINATION ${CMAKE_INSTALL_FULL_DATAROOTDIR}/metainfo)
-install (FILES ${CMAKE_SOURCE_DIR}/resources/images/icon.svg DESTINATION ${CMAKE_INSTALL_FULL_DATAROOTDIR}/icons/hicolor/scalable/apps RENAME plexmediaplayer.svg)
+install (FILES ${CMAKE_SOURCE_DIR}/resources/images/icon.svg DESTINATION ${CMAKE_INSTALL_FULL_DATAROOTDIR}/icons/hicolor/scalable/apps RENAME com.github.iwalton3.jellyfin-media-player.svg)

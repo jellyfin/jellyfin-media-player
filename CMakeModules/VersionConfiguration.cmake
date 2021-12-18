b68bf921da75caab1cd8b53a5bc5b150faa3d477
Ian Walton <ian@iwalton.com>
2021-12-18 08:59:07 -0500

    Update prerelease to (hopefully) work with WIX.

--- CMakeModules/VersionConfiguration.cmake
+++ CMakeModules/VersionConfiguration.cmake
@@ -21,13 +21,13 @@ else()
 endif()
 
 set(VERSION_MAJOR 1)
-set(VERSION_MINOR 6)
-set(VERSION_NANO 1)
+set(VERSION_MINOR 7)
+set(VERSION_NANO 0)
 
 option(UPGRADE_DEBUG "" OFF)
 
 set(VERSION_STRING "1.7.0-pre1")
-set(VERSION_STRING_SHORT "1.7.0-pre1")
+set(VERSION_STRING_SHORT "1.7.0")
 set(CANONICAL_VERSION_STRING "1.7.0-pre1")
 
 configure_file(src/core/Version.cpp.in src/core/Version.cpp)

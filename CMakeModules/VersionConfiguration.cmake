9baa52eebc1c3fd5669ae03a850fdc918958219e
Ian Walton <ian@iwalton.com>
2022-06-26 14:04:50 -0400

    Up version to 1.7.1.

--- CMakeModules/VersionConfiguration.cmake
+++ CMakeModules/VersionConfiguration.cmake
@@ -22,12 +22,12 @@ endif()
 
 set(VERSION_MAJOR 1)
 set(VERSION_MINOR 7)
-set(VERSION_NANO 0)
+set(VERSION_NANO 1)
 
 option(UPGRADE_DEBUG "" OFF)
 
-set(VERSION_STRING "1.7.0")
-set(VERSION_STRING_SHORT "1.7.0")
-set(CANONICAL_VERSION_STRING "1.7.0")
+set(VERSION_STRING "1.7.1")
+set(VERSION_STRING_SHORT "1.7.1")
+set(CANONICAL_VERSION_STRING "1.7.1")
 
 configure_file(src/core/Version.cpp.in src/core/Version.cpp)

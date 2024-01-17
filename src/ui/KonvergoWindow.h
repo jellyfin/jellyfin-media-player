9efd3718d5eb7aa4773d3813d2eb13244194fb0f
Baptiste Daroussin <bapt@FreeBSD.org>
2024-01-17 13:55:58 +0100

    Add support for FreeBSD

--- src/ui/KonvergoWindow.h
+++ src/ui/KonvergoWindow.h
@@ -50,7 +50,7 @@ public:
   bool isAlwaysOnTop()
   {
     Qt::WindowFlags forceOnTopFlags = Qt::WindowStaysOnTopHint;
-#ifdef Q_OS_LINUX
+#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
     forceOnTopFlags = forceOnTopFlags | Qt::X11BypassWindowManagerHint;
 #endif
     return (flags() & forceOnTopFlags);

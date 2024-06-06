1075ff722c32ada6102612c88b7fc1984d8686dd
Izzie Walton <izzie@iwalton.com>
2024-06-06 17:21:14 -0400

    Up version to 1.11.0.

--- CMakeModules/VersionConfiguration.cmake
+++ CMakeModules/VersionConfiguration.cmake
@@ -21,13 +21,13 @@ else()
 endif()
 
 set(VERSION_MAJOR 1)
-set(VERSION_MINOR 10)
-set(VERSION_NANO 1)
+set(VERSION_MINOR 11)
+set(VERSION_NANO 0)
 
 option(UPGRADE_DEBUG "" OFF)
 
-set(VERSION_STRING "1.10.1")
-set(VERSION_STRING_SHORT "1.10.1")
-set(CANONICAL_VERSION_STRING "1.10.1")
+set(VERSION_STRING "1.11.0")
+set(VERSION_STRING_SHORT "1.11.0")
+set(CANONICAL_VERSION_STRING "1.11.0")
 
 configure_file(src/core/Version.cpp.in src/core/Version.cpp)

3532a859571746ab6d921fba71af3bfe78bcddee
Skydreamer-123 <danielungar1@gmx.de>
2025-04-28 12:29:57 +0200

    adding Discord Settings Integration

--- src/discord/DiscordComponent.h
+++ src/discord/DiscordComponent.h
@@ -17,7 +17,9 @@ public:
   virtual bool componentInitialize() override;
   virtual const char* componentName() override;
   virtual bool componentExport() override;
-
+  Q_SLOT void valuesUpdated(const QVariantMap& values);
+  Q_SIGNAL void settingsUpdated(const QString& section, const QVariant& description);
+  
   discordpp::Client m_client;
 };
 

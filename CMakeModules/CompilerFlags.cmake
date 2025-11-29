0d7d0eb6b1a56c56aadc728d57e114adcd41048d
Andrew Rabert <ar@nullsum.net>
2025-11-28 23:14:26 -0500

    Remove explicit gold linker specification

--- CMakeModules/CompilerFlags.cmake
+++ CMakeModules/CompilerFlags.cmake
@@ -15,7 +15,6 @@ enable_if_supported(COMPILER_FLAGS_THIRD_PARTY "/wd4244")
 enable_if_supported(COMPILER_FLAGS_THIRD_PARTY "/wd4267")
 
 enable_if_links(LINK_FLAGS_RELEASE "-flto")
-enable_if_links(LINK_FLAGS "-fuse-ld=gold")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILER_FLAGS}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMPILER_FLAGS}")

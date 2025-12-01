107ab1affcb6d3525a6071ec46c11ca256f448a2
Andrew Rabert <ar@nullsum.net>
2025-11-30 22:11:53 -0500

    Revert "Remove non-dbus X11 power management"
    This reverts commit 7b067a751b56511441ad4c797d5387a8e0078e27.

---
+++ src/power/PowerComponentX11.cpp
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+
+#include "PowerComponentX11.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+PowerComponentX11::PowerComponentX11() : PowerComponent(0)
+{
+  m_timer = new QTimer(this);
+  connect(m_timer, &QTimer::timeout, this, &PowerComponentX11::onTimer);
+  m_timer->setInterval(15 * 1000);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void PowerComponentX11::onTimer()
+{
+  if (!m_process && !m_broken)
+  {
+    m_process = new QProcess(this);
+    m_process->setProcessChannelMode(QProcess::ForwardedChannels);
+    connect(m_process, (void (QProcess::*)(int,QProcess::ExitStatus))&QProcess::finished,
+            this, &PowerComponentX11::onProcessFinished);
+    connect(m_process, (void (QProcess::*)(QProcess::ProcessError))&QProcess::errorOccurred,
+            this, &PowerComponentX11::onProcessError);
+    m_process->start("xdg-screensaver", {"reset"});
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void PowerComponentX11::onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+  if (m_process)
+    m_process->deleteLater();
+  m_process = 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void PowerComponentX11::onProcessError(QProcess::ProcessError error)
+{
+  qCritical() << "Disabling screensaver is not working. Make sure xdg-screensaver is installed.";
+  m_broken = true;
+  onProcessFinished(-1, QProcess::CrashExit);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void PowerComponentX11::doDisableScreensaver()
+{
+  m_timer->start();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void PowerComponentX11::doEnableScreensaver()
+{
+  m_timer->stop();
+}
